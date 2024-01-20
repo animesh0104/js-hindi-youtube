2cb7d91bde2eaf827871d1223fe7b295ddb9d4e2
animesh0104 <157088047+animesh0104@users.noreply.github.com>
2024-01-20 11:38:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we're using nodejs not browser 
+// code readability should be high, different lines should be used
+
+console.log("animesh")
+
+let name = ("animesh")
+let age = 23
+let isloggedin = false
+
+// number => 2 to power 53
+// bigint => larger numbers
+// string => ""
+// boolean => true/false
+// null => standalone value   //output for typeof NULL is object
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
